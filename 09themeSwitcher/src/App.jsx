33f46f808e54bbb4595e5e94a98e491da4d9cfce
Kavish Ambani <kavishkkambani@gmail.com>
2024-08-06 09:18:17 +0530

    learning context api theme changer

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -12,23 +12,23 @@ function App() {
   const darkTheme = () => {
     setThemeMode("dark")
   }
-  useEffect(()=>{
+  useEffect(() => {
     document.querySelector("html").classList.remove("light", "dark");
     document.querySelector("html").classList.add(themeMode);
-  },[themeMode])
+  }, [themeMode])
   return (
-    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          <ThemeBtn/>
-        </div>
-          <Card/>
-        <div className="w-full max-w-sm mx-auto">
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+          <Card />
+          <div className="w-full max-w-sm mx-auto">
 
+          </div>
         </div>
       </div>
-    </div>
     </ThemeProvider>
   )
 }
--- 09themeSwitcher/src/context/theme.js
+++ 09themeSwitcher/src/context/theme.js
@@ -1,12 +1,12 @@
 import { createContext, useContext } from "react";
 export const ThemeContext = createContext({
     themeMode: "light",
-    darkTheme: () => {},
-    lightTheme: () => {},
+    darkTheme: () => { },
+    lightTheme: () => { },
 });
 
 export const ThemeProvider = ThemeContext.Provider
 
-export default function useTheme(){
+export default function useTheme() {
     return useContext(ThemeContext)
 }
